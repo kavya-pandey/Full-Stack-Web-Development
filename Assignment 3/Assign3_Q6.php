f09795077bcff3d7f81fbc6f590023f46ad8397c
Kavya Pandey <57443445+kavya-pandey@users.noreply.github.com>
2021-02-15 15:57:10 +0530

    Assignment 3

---
+++ Assignment 3/Assign3_Q6.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   
+    <title>WELCOME</title>
+    <script>
+    var firstName = prompt("Enter first Name");
+	var lastName = prompt("Enter last Name");
+	document.write("Welcome" + " " + firstName + " " + lastName + " to the World of Web"); 
+</script>
+</head>
+<body>
+</body>
+</html>
